b51d85917f17dc83f5889e21258ab2695ad7eddd
Gary Silas <gsilas@icloud.com>
2025-03-16 20:32:12 -0400

    add logout button; implement Log Out functionality in the page component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -17,6 +17,7 @@ const Home = async () => {
           await signOut({ redirectTo: ROUTES.SIGN_IN });
         }}
       >
+        {/* LogOut Button */}
         <Button type={"submit"}>Log Out</Button>
       </form>
     </>
